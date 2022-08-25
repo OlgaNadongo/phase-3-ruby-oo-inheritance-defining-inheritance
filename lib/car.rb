85b136ea9204a475aab28ca539ef8884f3149399
Olga Nadongo <winnienadongo@gmail.com>
2022-08-25 16:18:42 +0300

    complete lab

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,17 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+end
+
+Car.superclass
+Car.superclass.superclass
+Car.superclass.superclass.superclass.superclass
+Integer.superclass
+Car.class
+String.class
